b943f01d3ec21af4c9c8ed94b1c670822c356c4a
Jochem <jochemb@telfort.nl>
2016-01-28 22:39:37 +0100

    added first check

---
+++ LEDcheck.lua
@@ -0,0 +1,16 @@
+GPIO = require "GPIO";
+GPIO.setmode(GPIO.BOARD);
+GPIO.setup(7, GPIO.OUT);
+GPIO.setup(11, GPIO.IN);
+
+print("LED is off:")
+GPIO.output(7,false);
+print("Input is:", GPIO.input(11));
+print("Press any key to enable LED:");
+io.read()
+GPIO.output(7,true)
+os.execute("sleep 0.1") -- allow the input to go HIGH
+print("LED is on. Input is: ", GPIO.input(11))
+print("Press any key to quit")
+io.read()
+GPIO.output(7,false)
