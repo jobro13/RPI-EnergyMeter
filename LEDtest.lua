8e71d43ded33ed6275b0ea7da9018d2d72b065f2
Jochem <jochemb@telfort.nl>
2016-01-28 22:33:31 +0100

    first tests

--- LEDtest.lua
+++ LEDtest.lua
@@ -2,6 +2,10 @@ GPIO = require "GPIO"
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7,GPIO.OUT)
 
+print("Press any key to go HIGH")
+io.read()
+print("LED is now HIGH")
 GPIO.output(7,true)
-os.execute("sleep 1")
+print("Press another key when done")
+io.read()
 GPIO.output(7,false)
