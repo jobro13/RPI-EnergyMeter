05ec1dabd0ee78780cae75b1406eb26f3c45613f
Jochem <jochemb@telfort.nl>
2016-02-02 19:47:47 +0100

    MailConfig

---
+++ MailConfig.lua
@@ -0,0 +1,17 @@
+return {
+		-- Subject lines
+		Subject = {
+				Daily = "Energy Monitor: Daily report of %s";
+				Weekly = "Energy Monitor: Weekly report of %s";
+				Monthly = "Energy Monitor: Monthly report of %s";
+				Yearly = "Energy Monitor: Yearly report of %s;
+			},
+		-- Recipients of the mail
+		Recipients = {
+				"user1@host.com";
+				"user2@host.com";
+			},
+		-- 
+		Command = "cat %s | ssmtp %s";
+	}
+				
